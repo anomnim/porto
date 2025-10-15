470fa290e959f5fed0364296e05099c42368e404
anomnim <145103520+anomnim@users.noreply.github.com>
2025-10-15 23:31:52 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+(document).ready(function() {
+  $('#absen-masuk-btn').click(function() {
+    var nik = $('#nik').val();
+    var nama = $('#nama').val();
+    var lokasi = $('#lokasi').val();
+    var foto = $('#foto').val();
+    
+    // Kirim data ke Google Apps Script
+    $.ajax({
+      type: 'POST',
+      url: 'https:                                                  
+      data: {
+        '//script.google.com/macros/d/YOUR_SCRIPT_ID/exec',
+      data: {
+        'nik': nik,
+        'nama': nama,
+        'lokasi': lokasi,
+        'foto': foto,
+        'aksi': 'absen_masuk'
+      },
+      success: function(data) {
+        console.log(data);
+        $('#absensi-rekaman').html(data);
+      }
+    });
+  });
+  
+  $('#absen-pulang-btn').click(function() {
+    var nik = $('#nik').val();
+    var nama = $('#nama').val();
+    var lokasi = $('#lokasi').val();
+    var foto = $('#foto').val();
+    
+                                       
+    $.ajax({
+      type: '// Kirim data ke Google Apps Script
+    $.ajax({
+      type: 'POST',
+      url: 'https://script.google.com/macros/d/YOUR_SCRIPT_ID/exec',
+      data: {
+        'nik': nik,
+        'nama': nama,
+        'lokasi': lokasi,
+        'foto': foto,
+        'aksi': 'absen_pulang'
+      },
+      success: function(data) {
+        console.log(data);
+        $('#absensi-rekaman').html(data);
+      }
+    });
+  });
+});
